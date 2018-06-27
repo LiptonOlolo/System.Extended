d3e0fa733efe3fd71637b3c5cfade9ee0a7a2569
Alexander <zam.kuratora@gmail.com>
2018-06-27 20:07:57 +0300

    add

---
+++ System.Extended/System/LINQ/LinqExtended.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq
+{
+    /// <summary>
+    /// Расширения для IEnumerable
+    /// </summary>
+    public static class LinqExtended
+    {
+        /// <summary>
+        /// Проецирует каждый элемент всех последовательностей в 1 последовательность.
+        /// </summary>
+        /// <typeparam name="TSource">Тип элементов source.</typeparam>
+        /// <typeparam name="TResult">Тип элементов последовательности, возвращаемой selector.</typeparam>
+        /// <param name="source">Последовательность значений, которые следует проецировать.</param>
+        /// <param name="selector">Функция преобразования, применяемая к каждому элементу.</param>
+        /// <returns></returns>
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<IEnumerable<TSource>> source, Func<TSource, TResult> selector)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (selector == null)
+                throw new ArgumentNullException(nameof(selector));
+
+            foreach (var sub in source)
+                foreach (var item in sub)
+                    yield return selector(item);
+        }
+
+        /// <summary>
+        /// Проецирует каждый элемент всех последовательностей в 1 последовательность.
+        /// </summary>
+        /// <typeparam name="TSource">Тип элементов source.</typeparam>
+        /// <param name="source">Последовательность значений, которые следует проецировать.</param>
+        /// <returns></returns>
+        public static IEnumerable<TSource> SelectMany<TSource>(this IEnumerable<IEnumerable<TSource>> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            foreach (var sub in source)
+                foreach (var item in sub)
+                    yield return item;
+        }
+    }
+}
---
+++ Tests/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test[][] tests = new Test[][]
+            {
+                new Test[] {
+                    new Test(2, 2),
+                    new Test(2, 3)
+                },
+                new Test[] {
+                    new Test(3, 3),
+                    new Test(3, 4)
+                }
+            };
+
+
+            var res1 = tests.SelectMany(x => x.A);
+
+            foreach (var item in res1)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    class Test
+    {
+        public Test(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public int A { get; set; }
+
+        public int B { get; set; }
+    }
+}
